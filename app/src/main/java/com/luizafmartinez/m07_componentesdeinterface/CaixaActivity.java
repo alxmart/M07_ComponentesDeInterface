13dd04c8c7e723430721c5c7a193b412df3c106a
alxmart <alxmartinez@gmail.com>
2024-02-16 10:47:34 -0300

    Tests using R class

--- app/src/main/java/com/luizafmartinez/m07_componentesdeinterface/CaixaActivity.java
+++ app/src/main/java/com/luizafmartinez/m07_componentesdeinterface/CaixaActivity.java
@@ -3,6 +3,8 @@ package com.luizafmartinez.m07_componentesdeinterface;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CaixaActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class CaixaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caixa);
+
+        Button btnSalvar = findViewById(R.id.btn_salvar);
+
+        btnSalvar.setOnClickListener(v ->
+                System.out.println("Botão foi clicado")
+        );
+
     }
 }
